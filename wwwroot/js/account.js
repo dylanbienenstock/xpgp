9eb56048e6212d84bdd4280e658c40ff4887d450
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-02-27 23:26:22 -0800

    Add register errors

--- wwwroot/js/account.js
+++ wwwroot/js/account.js
@@ -1,7 +1,12 @@
-$("html, body").resize(() => {
-    // $("#logreg-inner").centerVertical();
-});
-
 $(() => {
-    // $("#logreg-inner").centerVertical();
+    $("html, body").resize(() => {
+        $("#register-errors").css({
+            width: $("#logreg-form").width() + 16,
+            height: $("#logreg-form").height() + 16,
+        });
+    });
+
+    $("#register-errors-close").click(() => {
+        $("#register-errors").fadeOut();
+    });
 });
