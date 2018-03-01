b4f48409c3e796ec6dd8e334a490b1ecaa7744cb
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-01 13:14:47 -0800

    Fix account menu bug

--- wwwroot/js/navbar.js
+++ wwwroot/js/navbar.js
@@ -32,10 +32,17 @@ function positionAccountMenu() {
     var $userInfo = $("#navbar-userinfo"); 
 
     if ($(document.body).width() > 768) { // Desktop
-        $accountMenu.width($userInfo.outerWidth()).offset({
-            left: $userInfo.offset().left,
-            top: $userInfo.height() - 20
-        });
+        if ($userInfo.outerWidth() < 175) {
+            $accountMenu.css({ width: "auto" }).offset({
+                left: $userInfo.offset().left + $userInfo.outerWidth() / 2 - $accountMenu.outerWidth() / 2,
+                top: $userInfo.height() - 20
+            });
+        } else {
+            $accountMenu.width($userInfo.outerWidth()).offset({
+                left: $userInfo.offset().left,
+                top: $userInfo.height() - 20
+            });
+        }
     } else { // Mobile
         $accountMenu.css({ width: "auto" });
 
