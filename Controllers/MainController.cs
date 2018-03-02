da5c356e04ab9eb3b64b75af80b5d659caa9aac8
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-03-02 13:42:50 -0800

    Remove old FormatTimeSpan function

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -23,24 +23,6 @@ namespace xpgp
             UserManager.SetDatabaseContext(_context);
         }
 
-        // public string FormatTimeSpan(TimeSpan span)
-        // {
-        //     if (span == TimeSpan.Zero) return "0 minutes";
-
-        //     var sb = new StringBuilder();
-
-        //     if (span.Days > 0)
-        //         sb.AppendFormat("{0} day{1}, ", span.Days, span.Days > 1 ? "s" : String.Empty);
-        //     if (span.Hours > 0)
-        //         sb.AppendFormat("{0} hour{1}, ", span.Hours, span.Hours > 1 ? "s" : String.Empty);
-        //     if (span.Minutes > 0)
-        //         sb.AppendFormat("{0} minute{1}, ", span.Minutes, span.Minutes > 1 ? "s" : String.Empty);
-
-        //     string formatted = sb.ToString();
-
-        //     return formatted.TrimEnd(new char[] { ',', ' ' });
-        // }
-
         public string FormatTimeSpan(TimeSpan span)
         {
             if (span.TotalMinutes <= 1) return "< 1 minute";
